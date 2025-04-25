f09a1a50e8c32aab2157c7e699e404fabc35d13f
Thet Paing Soe <thetpaingsoe.ptn@gmail.com>
2025-04-25 16:08:37 +0700

    Init

---
+++ features/w3/comments.py
@@ -0,0 +1,9 @@
+# This is single line comment
+print("Single Line Comment") # This is also same
+
+"""
+This is a multi-line comment
+This is another line of comment
+"""
+print("Multi Line Comment")  
+
---
+++ features/w3/variables.py
@@ -0,0 +1,63 @@
+# ---------------------------------------------
+# Basic Vairable Declaration
+# ---------------------------------------------
+print("Variable Declaration")
+
+x = 5
+y = "Theo"
+print(x)
+print(y)
+
+# ---------------------------------------------
+# Type Conversion from int to String
+# ---------------------------------------------
+print("\nType Conversion")
+
+x = 5
+x = "String"
+
+print(x)
+
+# ---------------------------------------------
+# Casting : If you want to specify the data type
+# of a variable, you can use casting.
+# ---------------------------------------------
+print("\nCasting")
+
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)    
+print(z)
+
+# ---------------------------------------------
+# Get the type of variable
+# ---------------------------------------------
+print("\nType of Variable")
+
+x = 5
+y = "5"
+print(type(x))
+print(type(y))
+
+# ---------------------------------------------
+# " and ' are same
+# ---------------------------------------------
+print("\nDouble and Single Quotes")
+
+x = "Theo"
+y = 'Leon'
+
+print(x)
+print(y)
+
+# ---------------------------------------------
+# Variable names are case-sensitive
+# ---------------------------------------------
+print("\nVariable Case Sensitivity")
+
+a = 1
+A = 2
+print(a)
+print(A)
