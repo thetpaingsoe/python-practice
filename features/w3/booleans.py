84323e08485298c5b81e03ff828c4a758d5100e0
Thet Paing Soe <thetpaingsoe.ptn@gmail.com>
2025-04-27 17:07:06 +0700

    Added Booleans

---
+++ features/w3/booleans.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------
+# Boolean
+# ---------------------------------------------
+print("Boolean")
+
+print(4 > 3) # True
+print(4 < 3) # False
+
+print(bool(False)) # False
+print(bool(True)) # True
+print(bool(0)) # False
+print(bool(1)) # True
+print(bool("")) # False
+print(bool(())) # False
+print(bool([])) # False
+print(bool({})) # False
+
+x = 23
+print(isinstance(x, int)) # True
