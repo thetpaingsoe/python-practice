78f103511ce1aed70d35961b99eb993e7ba61476
TPSoe <thetpaingsoe.ptn@gmail.com>
2025-11-21 16:23:33 +0700

    Feat : added q8

--- features/practice/q7.py
+++ features/practice/q7.py
@@ -1,5 +1,5 @@
 import unittest
-
+# 27. Remove Element
 # Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
 
 # Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
