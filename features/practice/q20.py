ffd7ed3580192898378ee58f78a39ad9df75d67e
TPSoe <thetpaingsoe.ptn@gmail.com>
2025-12-04 23:08:23 +0700

    feat : added q20

---
+++ features/practice/q20.py
@@ -0,0 +1,98 @@
+import unittest
+
+# 13. Roman to Integer
+
+# Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+# For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+# Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+# I can be placed before V (5) and X (10) to make 4 and 9. 
+# X can be placed before L (50) and C (100) to make 40 and 90. 
+# C can be placed before D (500) and M (1000) to make 400 and 900.
+# Given a roman numeral, convert it to an integer.
+
+# Example 1:
+
+# Input: s = "III"
+# Output: 3
+# Explanation: III = 3.
+# Example 2:
+
+# Input: s = "LVIII"
+# Output: 58
+# Explanation: L = 50, V= 5, III = 3.
+# Example 3:
+
+# Input: s = "MCMXCIV"
+# Output: 1994
+# Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+def romanToInt(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    symbolMap = {}
+    symbolMap['I'] = 1
+    symbolMap['V'] = 5
+    symbolMap['X'] = 10
+    symbolMap['L'] = 50
+    symbolMap['C'] = 100
+    symbolMap['D'] = 500
+    symbolMap['M'] = 1000
+    
+
+    total = 0
+            
+    for i in range(len(s)) : 
+        if s[i] == 'I' : 
+            if i + 1 < len(s) : 
+                if s[i + 1] == 'V' or s[i + 1] == 'X' : 
+                    total -= symbolMap[s[i]]
+                    continue
+                
+            total += symbolMap[s[i]]
+        
+        elif s[i] == 'X' : 
+            if i + 1 < len(s) : 
+                if s[i + 1] == 'L' or s[i + 1] == 'C' : 
+                    total -= symbolMap[s[i]]
+                    continue
+                
+            total += symbolMap[s[i]]
+
+        elif s[i] == 'C' : 
+            if i + 1 < len(s) : 
+                if s[i + 1] == 'D' or s[i + 1] == 'M' : 
+                    total -= symbolMap[s[i]]
+                    continue
+                
+            total += symbolMap[s[i]]
+        else : 
+            total += symbolMap[s[i]]
+
+
+    return total 
+
+class TestRomanToInt(unittest.TestCase) : 
+    def test_basic_case(self):
+        self.assertEqual(romanToInt("III"), 3)
+
+    def test_basic_negative(self):
+        self.assertEqual(romanToInt("LVIII"), 58)
+    
+    def test_basic_invalid_target(self):
+        self.assertEqual(romanToInt("MCMXCIV"), 1994)
+
+if __name__ == "__main__":
+    unittest.main()
