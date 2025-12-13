ce7dd8f1a9b7dc4eb599475b08a7ebffd458bb88
TPSoe <thetpaingsoe.ptn@gmail.com>
2025-12-13 23:08:13 +0700

    feat : added q29

---
+++ features/practice/q29.py
@@ -0,0 +1,82 @@
+import unittest
+
+# 205. Isomorphic Strings
+
+# Given two strings s and t, determine if they are isomorphic.
+
+# Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+# All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+# Example 1:
+
+# Input: s = "egg", t = "add"
+
+# Output: true
+
+# Explanation:
+
+# The strings s and t can be made identical by:
+
+# Mapping 'e' to 'a'.
+# Mapping 'g' to 'd'.
+# Example 2:
+
+# Input: s = "foo", t = "bar"
+
+# Output: false
+
+# Explanation:
+
+# The strings s and t can not be made identical as 'o' needs to be mapped to both 'a' and 'r'.
+
+# Example 3:
+
+# Input: s = "paper", t = "title"
+
+# Output: true
+
+# Constraints:
+
+# 1 <= s.length <= 5 * 104
+# t.length == s.length
+# s and t consist of any valid ascii character.
+
+def isIsomorphic(s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+    
+    sMap = {}
+    
+    for i in range(len(s)) : 
+        if s[i] in sMap : 
+            if sMap[s[i]] != t[i] : 
+                return False
+            
+        else : 
+            if t[i] in sMap.values() : 
+                return False
+            sMap[s[i]] = t[i]
+            
+
+    
+    return True
+
+class TestIsIsomorphic(unittest.TestCase) : 
+    def test_case1(self):
+        self.assertEqual(isIsomorphic("egg", "add"), True)
+
+    def test_case2(self):
+        self.assertEqual(isIsomorphic("foo", "bar"), False)
+
+    def test_case3(self):
+        self.assertEqual(isIsomorphic("paper", "title"), True)
+
+    def test_case4(self):
+        self.assertEqual(isIsomorphic("badc", "baba"), False)
+
+if __name__ == "__main__":
+    unittest.main()
